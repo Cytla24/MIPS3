e28ad2ff0806647d5575fbbe4930d51fd1a065d5
Cytla24 <ojuba2013@gmail.com>
2019-11-28 23:47:38 -0500

    changed to 1000 char

--- project3.s
+++ project3.s
@@ -1,17 +1,17 @@
 .data
 	comma: .asciiz ","
 	invv: .asciiz "NaN"
-	input: .space 6
+	input: .space 1000
 .text
 main:
 #input string
 	li $v0, 8
 	la $a0, input
-	li $a1, 6
+	li $a1, 1000
 	syscall
 	
 	addi $s0, $sp, 0
-	li $t6, 5
+	li $t6, 999
 push:
 	beq $t6, -1, done
 	
